9e6dddbf1f6723ecc123b1d457c694544a3fb782
Zoe Sirotiak <zmsiro@iastate.edu>
2026-05-20 11:26:35 -0500

    Initial commit

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Dbh2YtVljfZihrq6V6NpULficKGZ0W12zKHSlaHnevU/edit?gid=0#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyB9t-jY-yxWO88D2GWBIj4YMJIKoNXMdj8';
